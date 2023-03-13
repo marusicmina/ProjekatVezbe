715835d55f746c8d057399edcf16b62503321137
minamarusic <minamarusic@172.20.10.3>
2023-03-13 16:49:51 +0100

    Kreiranje projekta

---
+++ VezbePr/src/main/java/rvaVezbe/VezbePrApplication.java
@@ -0,0 +1,13 @@
+package rvaVezbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VezbePrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VezbePrApplication.class, args);
+	}
+
+}
---
+++ VezbePr/src/test/java/rvaVezbe/VezbePrApplicationTests.java
@@ -0,0 +1,13 @@
+package rvaVezbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VezbePrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
